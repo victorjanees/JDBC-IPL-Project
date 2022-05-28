130f695f6de8aa2144424814bf15c090342bf444
victorjanees <victorjanees@gmail.com>
2022-05-28 17:21:46 +0530

    scenario-1 completed

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         List<Match> matches = getMatchesData();
         List<Delivery> deliveries = getDeliveriesData();
 
-//        findNumberOfMatchesPlayedPerYear(matches);
+        findNumberOfMatchesPlayedPerYear(matches);
 //        findMatchesWonByEachTeamOverYears(matches);
 //        findExtraRunsConceededPerTeamIn2016(matches, deliveries);
 //        findEconomicalBowlerOf2015(matches, deliveries);
@@ -17,77 +17,90 @@ public class Main {
     public static List<Match> getMatchesData() {
         List<Match> matches = new ArrayList<>();
 
-        try{
-        String url = "jdbc:mysql://localhost:3306/IPL";
-        String username = "root";
-        String password = "Dhoni@007";
-        String query = "select * from Matches";
+        try {
+            String url = "jdbc:mysql://localhost:3306/IPL";
+            String username = "root";
+            String password = "Dhoni@007";
+            String query = "select * from Matches";
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, username, password);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, username, password);
 
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
 
-        while(rs.next()) {
+            while (rs.next()) {
 //            String matchData = rs.getString(2);
-            Match match = new Match();
+                Match match = new Match();
 
-            int matchId = Integer.parseInt(rs.getString(1));
-            int season = Integer.parseInt(rs.getString(2));
+                int matchId = Integer.parseInt(rs.getString(1));
+                int season = Integer.parseInt(rs.getString(2));
 
-            match.setMatchId(matchId);
-            match.setYear(season);
-            match.setWinner(rs.getString(11));
-            matches.add(match);
+                match.setMatchId(matchId);
+                match.setYear(season);
+                match.setWinner(rs.getString(11));
+                matches.add(match);
 
 //            st.close();
 //            con.close();
-        }
-        }
-        catch (Exception e){
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return matches;
-        }
+    }
 
-        public static List<Delivery> getDeliveriesData() {
-            List<Delivery> deliveries = new ArrayList<>();
+    public static List<Delivery> getDeliveriesData() {
+        List<Delivery> deliveries = new ArrayList<>();
 
-            try {
-                String url = "jdbc:mysql://localhost:3306/IPL";
-                String username = "root";
-                String password = "Dhoni@007";
-                String query = "select * from Deliveries";
+        try {
+            String url = "jdbc:mysql://localhost:3306/IPL";
+            String username = "root";
+            String password = "Dhoni@007";
+            String query = "select * from Deliveries";
 
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection con = DriverManager.getConnection(url, username, password);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, username, password);
 
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery(query);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
 
-                while (rs.next()) {
-                    Delivery delivery = new Delivery();
+            while (rs.next()) {
+                Delivery delivery = new Delivery();
 
-                    int matchId = Integer.parseInt(rs.getString(1));
-                    int extraRuns = Integer.parseInt(rs.getString(17));
-                    int totalRuns = Integer.parseInt(rs.getString(18));
-                    int batsmanRuns = Integer.parseInt(rs.getString(16));
+                int matchId = Integer.parseInt(rs.getString(1));
+                int extraRuns = Integer.parseInt(rs.getString(17));
+                int totalRuns = Integer.parseInt(rs.getString(18));
+                int batsmanRuns = Integer.parseInt(rs.getString(16));
 
-                    delivery.setExtraRuns(extraRuns);
-                    delivery.setTotalRuns(totalRuns);
-                    delivery.setMatchId(matchId);
-                    delivery.setBowlingTeam(rs.getString(4));
-                    delivery.setBowler(rs.getString(9));
-                    delivery.setBatsmanRuns(batsmanRuns);
-                    delivery.setBattingTeam(rs.getString(3));
-                    deliveries.add(delivery);
-                }
+                delivery.setExtraRuns(extraRuns);
+                delivery.setTotalRuns(totalRuns);
+                delivery.setMatchId(matchId);
+                delivery.setBowlingTeam(rs.getString(4));
+                delivery.setBowler(rs.getString(9));
+                delivery.setBatsmanRuns(batsmanRuns);
+                delivery.setBattingTeam(rs.getString(3));
+                deliveries.add(delivery);
             }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-            return deliveries;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return deliveries;
+    }
+
+    public static void findNumberOfMatchesPlayedPerYear(List<Match> matches) {
+        Map<Integer, Integer> matchesPerSeason = new HashMap<Integer, Integer>();
+        Iterator<Match> totalMatches = matches.iterator();
+        while (totalMatches.hasNext()) {
+            int year = totalMatches.next().getYear();
+            if (matchesPerSeason.containsKey(year)) {
+                matchesPerSeason.put(year, matchesPerSeason.get(year) + 1);
+            } else {
+                matchesPerSeason.put(year, 1);
+            }
+        }
+        System.out.println("Number Of Matches Played Per Season");
+        System.out.println(matchesPerSeason);
 
     }
+}
